0b368665a765d907adb504de382ca01e74e977e9
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-01-19 20:50:44 +0300

    Chakra UI style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { MainApp } from "./styled.js";
+import { Heading, Text, Button, Container, Center, Highlight } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
 
   const [count, setCount] = useState(retrieveCountValue());
 
-const addNumber = (count) => setCount(Number(count) + 1);
+  const addNumber = (count) => setCount(Number(count) + 1);
   // stop number at refresh
 
   useEffect(() => {
@@ -18,14 +18,34 @@ const addNumber = (count) => setCount(Number(count) + 1);
   }, [count]);
 
   return (
-    <MainApp>
+    <>
+      <Container maxW={['full', 'container.lg']}>
+        <Center>
+          <Heading lineHeight='tall'>
+            <Highlight
+            query='stranger'
+            styles={{px: '2', py: '1', rounded: 'full', bg: 'red.100' }}
+            >
+              Welcome stranger
+            </Highlight>
+          </Heading>
+        </Center>
 
-      <h1>Count App</h1>
-      <button onClick={() => addNumber(count)}>
-        count is {count}
-      </button>
+        <Center>
+          <Text color='tomato' fontSize='lg'>This is React counting app</Text>
+        </Center>
 
-    </MainApp>
+        <Center m='4'>
+          <Text fontSize='md'fontWeight='bold'>Count Now!</Text>
+        </Center>
+
+        <Center>
+          <Button colorScheme='teal' variant='solid' onClick={() => addNumber(count)}>
+            count is {count}
+          </Button>
+        </Center>
+      </Container>
+    </>
   )
 }
 
--- src/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const MainApp = styled.body`
-    background-color: #282c34;
-    display: flex;
-    justify-content: center;
-    width: 100vh; // 100% of viewport width
-    gap: 30px;
-`
